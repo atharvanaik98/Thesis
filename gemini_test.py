d7f48816e3cc442dd00e768c1b4a26e3f2088fae
firestorm98 <parthshriram7@gmail.com>
2024-02-06 15:01:43 +0100

    done

--- gemini_test.py
+++ gemini_test.py
@@ -1,18 +1,18 @@
-# %%
 import vertexai
+from pathlib import Path
+import urllib
 from vertexai.preview.generative_models import GenerativeModel, ChatSession
-vertexai.init(project="totemic-veld-412608")
+vertexai.init(project = "totemic-veld-412608")
 def remove_code_fences(text):
     lines = text.split('\n')
     lines = [line for line in lines if not line.strip().startswith('```')]
     return '\n'.join(lines)
 
+here = Path(__file__).parent
+context = (here / "Input.txt").read_text()
+
 model = GenerativeModel("gemini-pro")
 chat = model.start_chat()
-
-file = open(r"C:\Users\yy56559\Downloads\Input.txt", "r")
-context = file.read()
-
 
 chat.send_message(context)
 #Input the prompt here
@@ -23,10 +23,3 @@ response = chat.send_message(prompt)
 
 print()
 print(remove_code_fences(response.text))
-
-
-
-
-
-
-
