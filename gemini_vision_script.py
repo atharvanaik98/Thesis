8739b9bde4d6717552d7b2e3a7747978257757ff
firestorm98 <parthshriram7@gmail.com>
2024-02-21 10:21:26 +0100

    new files added

---
+++ gemini_vision_script.py
@@ -0,0 +1,60 @@
+import yaml
+import vertexai
+import requests
+from pathlib import Path
+from vertexai.preview.generative_models import GenerativeModel, Image
+
+#initiate the vertex AI with project ID 
+vertexai.init(project = "totemic-veld-412608")
+
+#function definition to remove code fences from the printed output 
+def remove_code_fences(text):
+    lines = text.split('\n')
+    lines = [line for line in lines if not line.strip().startswith('```')]
+    return '\n'.join(lines)
+
+def appendtoyaml(filepath, response):
+    with open (filepath, 'w') as yamlfile:
+        yaml.safe_dump(response)
+ 
+
+here = Path(__file__).parent
+
+#Append the contents of the input file to a variable named context
+context = (here / "Input.txt").read_text()
+
+#path to the yaml file
+filepath = (here / "query.yml")
+ 
+#provide the model with the image that needs to be analysed.
+image_file = (here / "exhaust_case.jpg")
+image = Image.load_from_file(image_file)
+
+#select the generative model 
+model = GenerativeModel("gemini-1.0-pro-vision")
+
+#initiate interactive chat environment 
+chat = model.start_chat()
+
+#generate model config. Higher temperature prompts the model to generate more creative content. 
+config = {
+    "temperature": 0.0,
+}
+
+#Input the prompt here
+while True: 
+    prompt = input("Enter the description of the geometry you want to select: ")
+    if prompt == str('cancel'):
+        break 
+    else: 
+    #Generate a response based on the prompt
+        response = model.generate_content([image, context, prompt], generation_config=config)
+
+        print()
+        #print model response
+        print(response.text)
+
+        response = response.text
+        appendtoyaml(filepath, response)
+
+
