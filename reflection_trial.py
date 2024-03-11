cf7a625b7d486770482a5e106e303dad5cbe2c78
firestorm98 <parthshriram7@gmail.com>
2024-03-11 14:59:51 +0100

    ssl certificate validation problem unresolved. tried generating reflection prompt model

--- reflection_trial.py
+++ reflection_trial.py
@@ -1,37 +1,15 @@
-# Import necessary packages
 from langchain_openai import OpenAI
-from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from pathlib import Path
 from langchain_core.output_parsers import StrOutputParser
 
-# Function definitions
-
-#function to call a path to the file and read it. 
 def context_gen(file_name):
     Folder = "VertexLang"
     here = Path(locals().get('__file__', Folder)).resolve()
     parameter = (here / file_name).read_text()
     return parameter
-
-#remove code fences from the output
-def remove_code_fences(text):
-    lines = text.split("\n")
-    lines = [line for line in lines if not line.strip().startswith('```')]
-    lines[0] = lines[0].replace(' -', '-', 1)
-    return "\n".join(lines)
-
-#invoke and run the model with the given prompt
-def test_openai(few_shot_prompt):
-    generate = ChatPromptTemplate.from_messages([("system", system), few_shot_prompt, ("human", human),])
-    chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
-    #select an output parser
-    output_parser = StrOutputParser()
-    chain = generate | chat | output_parser
-    result = chain.invoke({}, {"tags": ["loop 001"]})
-    print(remove_code_fences(result))
-    print()
-    return
 
 #Inputs to the model
 
@@ -51,8 +29,6 @@ schema = context_gen("dataBaseSchema.txt")
 '''change the human variable to test different inputs'''
 human = ("select the blend in the xz plane between the angle 45 and 90 and then select the connected blend")
 
-
-# Few shot prompts example template 
 examples = [
     {"input": input_examples, "output": output_examples},
 ]
@@ -68,7 +44,13 @@ few_shot_prompt = FewShotChatMessagePromptTemplate(
     examples=examples,
 )
 
-'''Function call to run the model'''
-test_openai(few_shot_prompt)
+prompt = ChatPromptTemplate.from_messages([("system", system), few_shot_prompt, MessagesPlaceholder(variable_name="messages"),])
+chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
+chain = prompt | chat
 
-
+yaml = ""
+request = HumanMessage(content = human)
+for chunk in chain.stream({"messages": [request]}):
+    print(chunk.content, end="")
+    yaml += chunk.content
+    
