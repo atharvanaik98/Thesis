63ccba1fe746ba8453573beeeac307b11e6cf33f
firestorm98 <parthshriram7@gmail.com>
2024-03-28 08:41:54 +0100

    Moving on to working on RAG this point onwards

--- vdb.py
+++ vdb.py
@@ -1,15 +1,6 @@
-from langchain_community.document_loaders import DirectoryLoader
-from langchain_community.vectorstores import Chroma
-from langchain_text_splitters import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 
 
-loader = DirectoryLoader("./Context_files")
-docs = loader.load()
 
-splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
-split_docs = splitter.split(docs)
 
-vectorstore = Chroma.from_documents(documents = split_docs, embedding=OpenAIEmbeddings())
-retriever = vectorstore.as_retriever()
+
  
