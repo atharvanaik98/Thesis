69e589aea3b1184359ae4fa572f3c7dd2c5768d9
firestorm98 <parthshriram7@gmail.com>
2024-05-17 20:59:11 +0200

    preparing for phase

--- openlang/openlang.py
+++ openlang/openlang.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptT
 from langchain_openai import ChatOpenAI
 from pathlib import Path
 from langchain_core.output_parsers import StrOutputParser
+from langchain_anthropic import ChatAnthropic
 import vertexai
 vertexai.init(project = "simcenter-llm-trial")
 # Function definitions
@@ -23,15 +24,9 @@ def remove_code_fences(text):
     return "\n".join(lines)
 
 #invoke and run the model with the given prompt
-<<<<<<< Updated upstream
-def test_openai(few_shot_prompt):
-    generate = ChatPromptTemplate.from_messages([("system", system), few_shot_prompt, ("human", human),])
-    chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-=======
 def test_openai(human_input):
     generate = ChatPromptTemplate.from_messages([("system", system), few_shot_prompt, ("human", human_input),])
     chat = ChatOpenAI(model="gpt-4", temperature=0.0)
->>>>>>> Stashed changes
     #select an output parser
     output_parser = StrOutputParser()
     chain = generate | chat | output_parser
@@ -42,7 +37,7 @@ def test_openai(human_input):
 
 def test_gemini(human_input):
     generate = ChatPromptTemplate.from_messages([("system", system), few_shot_prompt, ("human", human_input),])
-    chat = ChatVertexAI(model="gemini-1.0-pro", temperature=0.5, convert_system_message_to_human=False)
+    chat = ChatVertexAI(model="gemini-1.0-pro", temperature=0.0, convert_system_message_to_human=False)
     #select an output parser
     output_parser = StrOutputParser()
     chain = generate | chat | output_parser
@@ -50,6 +45,18 @@ def test_gemini(human_input):
     print(remove_code_fences(result))
     print()
     return
+
+def test_claude(human_input):
+    generate = ChatPromptTemplate.from_messages([("system", system), few_shot_prompt, ("human", human_input),])
+    chat = ChatAnthropic(model_name="claude-3-sonnet-20240229", temperature=0.0) # type: ignore
+    #select an output parser
+    output_parser = StrOutputParser()
+    chain = generate | chat | output_parser
+    result = chain.invoke({}, {"tags": ["loop 001"]})
+    print(remove_code_fences(result))
+    print()
+    return
+
 #Inputs to the model
 
 #create inputs to the model, telling it what needs to be done. 
@@ -64,10 +71,6 @@ input_examples = context_gen("inputex.txt")
 
 #provides the database schema
 schema = context_gen("dataBaseSchema.txt")
-
-'''change the human variable to test different inputs'''
-human = ("select all flanges in the aftmost section of the model")
-
 
 # Few shot prompts example template 
 examples = [
@@ -91,6 +94,5 @@ human_input = "Select all strut fillets in the aftmost section of the model"
 
 test_runs = [1, 2, 3, 4, 5]
 for i in test_runs:
-    test_openai(human_input)
-
-
+    test_claude(human_input)
+    print()
